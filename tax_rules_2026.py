dee9edf724fb0847c6c64cae09e59f83cd2e2ca1
mukhtaraabbasglobalent-creator <mukhtaraabbasglobalent@gmail.com>
2026-01-24 19:28:32 +0100

    Create tax_rules_2026.py
    "Add 2026 simulated tax rules"

---
+++ tax_rules_2026.py
@@ -0,0 +1,12 @@
+# Simulated Nigeria Tax Reform Rules (2026)
+
+PERSONAL_ALLOWANCE = 200000  # yearly tax-free amount in Naira
+
+TAX_BRACKETS = [
+    (300000, 0.07),
+    (300000, 0.11),
+    (500000, 0.15),
+    (500000, 0.19),
+    (1600000, 0.21),
+    (float('inf'), 0.24)
+]
